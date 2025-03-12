62fdf9e928602c25c6e5b288586e68e107ccf7c9
Tiziano Montenegro <tizianomontenegro07@gmail.com>
2025-03-12 12:36:22 +0100

    sample_pagerank funcion logic is done, Markov Model implemented

--- pagerank.py
+++ pagerank.py
@@ -66,7 +66,7 @@ def transition_model(corpus, page, damping_factor):
 
     """Get all possible path from the current page"""
     possible_pages = corpus.get(page)
-    print(possible_pages)
+    # print(possible_pages)
 
     """If there are possible pages"""
     if len(possible_pages) > 0:
@@ -106,7 +106,108 @@ def sample_pagerank(corpus, damping_factor, n):
     their estimated PageRank value (a value between 0 and 1). All
     PageRank values should sum to 1.
     """
-    raise NotImplementedError
+    """The object that the function has to return"""
+    pagerank = dict()
+    
+    """Counter for keep track of the samples generated"""
+    counter = 0
+    
+    """Variable to keep track of sample each page"""
+    next_page = None
+
+    """List to save and keep track of all samples"""
+    samples_list = list()
+
+    """This code will be executed n times adding sample to samples list"""
+    while counter < n:
+        counter += 1
+        
+        if next_page is None:
+            """This gets randomly a page from the corpus"""
+            random_page = random.choice(list(corpus.keys()))
+
+            """The inital sample to work next others"""
+            first_sample = transition_model(corpus,random_page,damping_factor)
+
+            """Add first sample to samples list"""
+            samples_list.append(first_sample)
+
+            """Set the next page for the next sample"""
+            next_page = get_next_page(first_sample)
+
+        else:
+            """Create a sample"""
+            nsample = transition_model(corpus,next_page,damping_factor)
+
+            """Add that sample to the samples list"""
+            samples_list.append(nsample)
+
+            """Update the next page for the next sample"""
+            next_page = get_next_page(nsample)
+    
+    # print(samples_list)
+    """Quantity of pages in corpus"""
+    qpages = len(list(corpus.keys()))
+    # print(qpages)
+    total = 0
+    # sample_tracker = list()
+    weights_sum = list()
+    # weights_len = 0
+    
+    """
+    Set values 0 in positions page index where the weights of this page has to be sum
+    The index of each value depends of the quantity of pages in corpus
+    """
+    for page in range(qpages):
+        weights_sum.insert(page, 0)
+
+    """Loop all the sample we have save in samples list"""
+    for sample in samples_list:
+        # sample_pages = list(sample.keys())
+        """Getting the probability of each page in a list"""
+        sample_weights = list(sample.values())
+        
+        # weights_len += 1
+
+        """
+        This sums all the weights of each page in different position
+        e.g. [total_weight_page_1, total_weight_page_2, total_weight_page_3]
+        """
+        for page, weight in enumerate(sample_weights):
+            weights_sum[page] += weight
+
+    # print(weights_sum)
+    # print(weights_len)
+    
+    """
+    Dividing each weight of each page by the 
+    damping factor to get the average
+    Dividing each "total_weight_page_n"
+    """
+    for page in range(qpages):
+        weights_sum[page] = weights_sum[page] / n
+        total += weights_sum[page]
+
+    # print(weights_sum)
+    # print("total", total)
+
+    """Join all the pages in the corpus with their weights"""
+    for page, weight in zip(list(corpus.keys()),weights_sum):
+        # print(page, weight)
+
+        """Add the page to the pagerank with its weights as value"""
+        pagerank[page] = weight
+
+    # print(pagerank)
+    """Return the final pagerank results"""
+    return pagerank
+
+
+def get_next_page(sample):
+    sample_pages = list(sample.keys())
+    sample_weights = list(sample.values())
+    next_page = random.choices(sample_pages,sample_weights, k=1)
+    return next_page[0]
 
 
 def iterate_pagerank(corpus, damping_factor):
